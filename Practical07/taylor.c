624d85c96b973c6a67cafea9fcc89dd462ec982f
Eddie Kelly <eddiek106@gmail.com>
2023-11-20 19:38:30 +0000

    Added some more comment codes to the taylor.c file to explain how the taylor approximation of e is created

--- Practical07/taylor.c
+++ Practical07/taylor.c
@@ -22,13 +22,18 @@ int main(void){
         terms[i] = 1.0/((double)factorial(i+1));
         printf("e term for order %d is %1.14lf \n",i+1,terms[i]);
     }
-
+/* Start summing up the series, we ignore the first term/
+ * as it is always */
     e = 1.0;
     for(i=0;  i<order; i++){
         e = e + terms[i];
     }
-    free(terms);
+    /*Free up the memory that is used to store the
+     * the taylor terms in the approximation*/
 
+    free(terms);
+    /* Print the approximate value of e from the taylor series and /
+     * compare to the value of e as as stored by the math.h library*/
     printf("e is estimated as %.10lf, with difference %e\n", e,e-exp(1.0));
 
     return 0;
@@ -36,6 +41,10 @@ int main(void){
 
 int factorial(int n){
     if(n<0){
+	/*As the factorial function is only defined for positive integers/
+	 * we check that input is a positve integer and if not return an/
+	 * error message*/
+
         printf("Error: Negative number passed to factorial function.\n");
         return(-1);
 
